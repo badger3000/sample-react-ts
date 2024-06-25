9d27f0de95805da7f8c45128be2723d71292e3a7
Kyle Ross <kyle@badger3000.com>
2024-06-25 07:49:51 -0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {QueryClient, QueryClientProvider} from "react-query";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
