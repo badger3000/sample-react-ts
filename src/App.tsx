29623e8f140701c110b1a80ce31a2516b271a94f
Kyle Ross <kyle@badger3000.com>
2024-06-25 08:08:31 -0700

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useQuery} from "react-query";
+
+const fetchPhotos = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    data: photos,
+    isLoading,
+    isError,
+  } = useQuery(["photos", fetchPhotos], fetchPhotos);
 
+  if (isLoading) {
+    <div>Loading...</div>;
+  }
+  if (isError) {
+    <div>Error: dude, its broken.</div>;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <section className="conteiner w-full bg-slate-500">
+      <pre className="text-sm font-bold underline">
+        {JSON.stringify(photos, null, 2)}
+      </pre>
+    </section>
+  );
 }
 
-export default App
+export default App;
